a02f75fe5a5eef4d68e8675448c08f7cb293762e
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2025-09-25 19:56:31 -0500

    Add files via upload

---
+++ ProfªPriscillaAbreu/PrincipalLista2Exer3.java
@@ -0,0 +1,26 @@
+package orientacaoPriscila;
+import orientacaoPriscila.Lista2Exer3;
+import java.util.Scanner;
+public class PrincipalLista2Exer3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner triangulos = new Scanner(System.in);
+		
+		Lista2Exer3 triangulo = new Lista2Exer3(10, 15, 20);
+		triangulo.exibirDados();
+		
+		Lista2Exer3 triangulo2 = new Lista2Exer3(0, 0, 0);
+		triangulo2.exibirDados();
+		
+		Lista2Exer3 triangulo3 = new Lista2Exer3(10, 15, 10);
+		triangulo3.exibirDados();
+		
+		
+		
+		
+
+	}
+
+}
