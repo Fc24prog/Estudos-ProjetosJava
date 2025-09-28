2b76b596500ad78899275ed884ae95bdbb6b9df3
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2025-09-28 16:18:59 -0300

    Update Lista2Exer4.java

--- ProfªPriscillaAbreu/Lista2Exer4.java
+++ ProfªPriscillaAbreu/Lista2Exer4.java
@@ -15,32 +15,47 @@ public class Lista2Exer4 {
 		this.nota1 = nota1;
 		this.nota2 = nota2;
 	}
-	public String getNome() 
-	{
+	
+   public String getNome() 
+   {
 		return nome;
 	}
-	
-	public String getMatricula()
+
+	public void setNome(String nome)
 	{
-		return matricula;
+		this.nome = nome;
 	}
 	
+    public String getMatricula() 
+    {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula)
+	{
+		this.matricula = matricula;
+	}
+
 	public double getNota1() 
 	{
 		return nota1;
 	}
-	public void setNota1(double nota1)
+
+	public void setNota1(double nota1) 
 	{
 		this.nota1 = nota1;
 	}
-	public double getNota2()
+	
+	public double getNota2() 
 	{
 		return nota2;
 	}
-	public void setNota2(double nota2) 
+
+	public void setNota2(double nota2)
 	{
 		this.nota2 = nota2;
 	}
+
 	public double calcularMedia() 
 	{
 		return (this.nota1 + this.nota2) / 2;
